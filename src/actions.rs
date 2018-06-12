2b4e143165d8829ddeb7396648fd0609a3bcc31f
Michael Murphy <mmstickman@gmail.com>
2018-06-12 10:58:35 -0600

    Make git actions generic

--- src/actions.rs
+++ src/actions.rs
@@ -46,6 +46,33 @@ fn git_cmd(args: &[&str], name: &str) -> Result<String, (String, GitError)> {
     }
 }
 
+fn parallel_process<M, E, N>(repos: Vec<Repo>, flags: u8, namespace: &str, message: M, exists: E, new: N)
+where M: Sync + Send + Fn(&Repo),
+      E: Sync + Send + Fn(&Repo, &str) -> Result<String, (String, GitError)>,
+      N: Sync + Send + Fn(&Repo, &str) -> Result<String, (String, GitError)>,
+{
+    let results = repos
+        .par_iter()
+        .filter(|repo| namespace == "" || repo.namespace == namespace)
+        .inspect(|repo| message(repo))
+        .map(|repo| {
+            let url = if flags & 0b01 != 0 { repo.get_ssh_url() } else { repo.get_url() };
+            if Path::new(&repo.name).exists() {
+                exists(repo, url)
+            } else {
+                new(repo, url)
+            }
+        })
+        .collect::<Vec<Result<String, (String, GitError)>>>();
+
+    for result in results {
+        match result {
+            Ok(repo) => println!("successfully cloned {}", repo),
+            Err((repo, why)) => println!("failed to clone {}: {}", repo, why)
+        }
+    }
+}
+
 pub trait GitAction {
     fn get_repos(&self) -> Vec<Repo>;
 
@@ -61,111 +88,68 @@ pub trait GitAction {
     }
 
     fn clone(&self, flags: u8, namespace: &str) {
-        let results = self.get_repos()
-            .par_iter()
-            .filter(|repo| namespace == "" || repo.namespace == namespace)
-            .inspect(|repo| println!("cloning {} from {}", repo.name, repo.get_url()))
-            .map(|repo| if !Path::new(&repo.name).exists() {
-                let url = if flags & 0b01 != 0 { repo.get_ssh_url() } else { repo.get_url() };
-                git_cmd(&["clone", "--recursive", url, &repo.name], &repo.name)
-            } else {
-                Ok(repo.name.clone())
-            })
-            .collect::<Vec<Result<String, (String, GitError)>>>();
-
-        for result in results {
-            match result {
-                Ok(repo) => println!("successfully cloned {}", repo),
-                Err((repo, why)) => println!("failed to clone {}: {}", repo, why)
-            }
-        }
+        parallel_process(
+            self.get_repos(),
+            flags,
+            namespace,
+            // Message to print before processing repo
+            |repo| println!("cloning {} from {}", repo.name, repo.get_url()),
+            // If repo already exists, do this
+            |repo, _url| Ok(repo.name.clone()),
+            // Else, do this
+            |repo, url| git_cmd(&["clone", "--recursive", url, &repo.name], &repo.name)
+        )
     }
 
     fn pull(&self, flags: u8, namespace: &str) {
-        let results = self.get_repos()
-            .par_iter()
-            .filter(|repo| namespace == "" || repo.namespace == namespace)
-            .inspect(|repo| println!("pulling {} from {}", repo.name, repo.get_url()))
-            .map(|repo| if !Path::new(&repo.name).exists() {
-                let url = if flags & 0b01 != 0 { repo.get_ssh_url() } else { repo.get_url() };
-                git_cmd(&["clone", "--recursive", url, &repo.name], &repo.name)
-            } else {
+        parallel_process(
+            self.get_repos(),
+            flags,
+            namespace,
+            |repo| println!("pulling {} from {}", repo.name, repo.get_url()),
+            |repo, _url| {
                 git_cmd(&["-C", &repo.name, "pull"], &repo.name)
                     .and_then(|_| git_cmd(&["-C", &repo.name, "submodule", "sync", "--recursive"], &repo.name))
                     .and_then(|_| git_cmd(&["-C", &repo.name, "submodule", "update", "--init"], &repo.name))
-            })
-            .collect::<Vec<Result<String, (String, GitError)>>>();
-
-        for result in results {
-            match result {
-                Ok(repo) => println!("successfully pulled {}", repo),
-                Err((repo, why)) => println!("failed to pull {}: {}", repo, why)
-            }
-        }
+            },
+            |repo, url| git_cmd(&["clone", "--recursive", url, &repo.name], &repo.name)
+        )
     }
 
     fn checkout(&self, flags: u8, namespace: &str) {
-        let results = self.get_repos()
-            .par_iter()
-            .filter(|repo| namespace == "" || repo.namespace == namespace)
-            .inspect(|repo| println!("checking out {} from {}", repo.name, repo.get_url()))
-            .map(|repo| if !Path::new(&repo.name).exists() {
-                let url = if flags & 0b01 != 0 { repo.get_ssh_url() } else { repo.get_url() };
-                git_cmd(&["clone", "--recursive", url, &repo.name], &repo.name)
-            } else {
+        parallel_process(
+            self.get_repos(),
+            flags,
+            namespace,
+            |repo| println!("checking out {} from {}", repo.name, repo.get_url()),
+            |repo, _url| {
                 git_cmd(&["-C", &repo.name, "fetch", "origin"], &repo.name)
                     .and_then(|_| git_cmd(&["-C", &repo.name, "submodule", "sync", "--recursive"], &repo.name))
                     .and_then(|_| git_cmd(&["-C", &repo.name, "submodule", "update", "--init", "--recursive"], &repo.name))
-            })
-            .collect::<Vec<Result<String, (String, GitError)>>>();
-
-        for result in results {
-            match result {
-                Ok(repo) => println!("successfully pulled {}", repo),
-                Err((repo, why)) => println!("failed to pull {}: {}", repo, why)
-            }
-        }
+            },
+            |repo, url| git_cmd(&["clone", "--recursive", url, &repo.name], &repo.name)
+        )
     }
 
     fn mirror_pull(&self, flags: u8, namespace: &str) {
-        let results = self.get_repos()
-            .par_iter()
-            .filter(|repo| namespace == "" || repo.namespace == namespace)
-            .inspect(|repo| println!("pulling mirror {} from {}", repo.name, repo.get_url()))
-            .map(|repo| {
-                let url = if flags & 0b01 != 0 { repo.get_ssh_url() } else { repo.get_url() };
-                if !Path::new(&repo.name).exists() {
-                    git_cmd(&["clone", "--mirror", url, &repo.name], &repo.name)
-                } else {
-                    git_cmd(&["-C", &repo.name, "fetch", url], &repo.name)
-                }
-            })
-            .collect::<Vec<Result<String, (String, GitError)>>>();
-
-        for result in results {
-            match result {
-                Ok(repo) => println!("successfully pulled mirror {}", repo),
-                Err((repo, why)) => println!("failed to pull mirror {}: {}", repo, why)
-            }
-        }
+        parallel_process(
+            self.get_repos(),
+            flags,
+            namespace,
+            |repo| println!("pulling mirror {} from {}", repo.name, repo.get_url()),
+            |repo, url| git_cmd(&["-C", &repo.name, "fetch", url], &repo.name),
+            |repo, url| git_cmd(&["clone", "--mirror", url, &repo.name], &repo.name)
+        )
     }
 
     fn mirror_push(&self, flags: u8, namespace: &str) {
-        let results = self.get_repos()
-            .par_iter()
-            .filter(|repo| namespace == "" || repo.namespace == namespace)
-            .inspect(|repo| println!("pushing mirror {} to {}", repo.name, repo.get_url()))
-            .map(|repo| {
-                let url = if flags & 0b01 != 0 { repo.get_ssh_url() } else { repo.get_url() };
-                git_cmd(&["-C", &repo.name, "push", "--mirror", url], &repo.name)
-            })
-            .collect::<Vec<Result<String, (String, GitError)>>>();
-
-        for result in results {
-            match result {
-                Ok(repo) => println!("successfully pushed mirror {}", repo),
-                Err((repo, why)) => println!("failed to pushed mirror {}: {}", repo, why)
-            }
-        }
+        parallel_process(
+            self.get_repos(),
+            flags,
+            namespace,
+            |repo| println!("pushing mirror {} from {}", repo.name, repo.get_url()),
+            |repo, url| git_cmd(&["-C", &repo.name, "push", "--mirror", url], &repo.name),
+            |repo, url| git_cmd(&["-C", &repo.name, "push", "--mirror", url], &repo.name)
+        )
     }
 }
